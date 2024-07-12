b64ea7838b363c749794d1e5e2e87795aebbd978
Carol Fagundes <carolinarftk@gmail.com>
2024-07-12 16:01:25 -0300

    criando rota para posts

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -6,16 +6,18 @@ export default function Banner() {
   return (
     <div className={styles.banner}>
       <div className={styles.apresentacao}>
-        <h1 className={styles.titulo}>Olá mundo!</h1>
+        <h1 className={styles.titulo}>Olá, Mundo!</h1>
         <p className={styles.paragrafo}>
-          Sou Carolina, 19 anos, do Rio Grande do Sul. No terceiro semestre de
-          Análise e Desenvolvimento de Sistemas e Analista de Ativação na
-          Unifique. Foco em tecnologias Web-Frontend (HTML5, CSS3, Javascript) e
-          Back-end (POO em Java).
+          Meu nome é Carolina, tenho 19 anos e sou do Rio Grande do Sul. Estou
+          no terceiro semestre de Análise e Desenvolvimento de Sistemas no
+          Instituto Federal Sul-rio-grandense e trabalho como Analista de
+          Ativação na Unifique, configurando roteadores e RBs para clientes
+          corporativos. Tenho foco em tecnologias Web-Frontend (HTML5, CSS3,
+          Javascript) e estou aprimorando minhas habilidades em Back-end,
+          especialmente em Programação Orientada a Objetos com Java.
         </p>
       </div>
       <div className={styles.imagens}>
-
         <img
           className={styles.minhaFoto}
           src={minhaFoto}
--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,15 +1,14 @@
 import styles from "./Inicio.module.css";
 import posts from "json/post.json";
-import Post from "componentes/Post";
+import PostCard from "componentes/PostCard";
 
 export default function Inicio() {
   return (
     <main>
-
       <ul className={styles.posts}>
         {posts.map((post) => (
           <li key={post.id}>
-            <Post post={post} />
+            <PostCard post={post} />
           </li>
         ))}
       </ul>
